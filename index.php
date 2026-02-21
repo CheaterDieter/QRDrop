ebe19601d4d54bfefacb49d9b5b5102363530b03
CheaterDieter <mail@david-heger.de>
2026-02-21 13:17:35 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-define('APP_VERSION', '1.1.0'); // App-Version
+define('APP_VERSION', '1.1.1'); // App-Version
 
 if (file_exists(__DIR__ . '/config.php')) {
     require_once __DIR__ . '/config.php';
