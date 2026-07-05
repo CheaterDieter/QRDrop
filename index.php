40ad45fda282bf5b361f5c1a4ed006253dc32ed5
CheaterDieter <mail@david-heger.de>
2026-07-05 13:32:35 +0200

    Bump app version to 1.1.2

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-define('APP_VERSION', '1.1.1'); // App-Version
+define('APP_VERSION', '1.1.2'); // App-Version
 
 if (file_exists(__DIR__ . '/config.php')) {
     require_once __DIR__ . '/config.php';
